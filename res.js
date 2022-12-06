d5c5eb405247727c6631347a0b0272c143608527
Chandan Mali <88288584+Chandanmali@users.noreply.github.com>
2022-12-06 16:39:36 +0530

    main-2

---
+++ res.js
@@ -0,0 +1,13 @@
+burger = document.querySelector('.burger');
+mainnav = document.querySelector('.main-nav')
+menu = document.querySelector('.menu')
+
+
+
+burger.addEventListener('click', ()=>{
+       // mainnav.classList.toggle('v-class-resp');
+       menu.classList.toggle('v-class-resp');
+       mainnav.classList.toggle('h-nav-resp')
+
+
+})
